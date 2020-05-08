22043449f07c6ce204d08cc62eb7df779911c9da
Travis Gandy <travis_gandy@yahoo.com>
2020-05-08 03:50:50 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,22 +1,16 @@
 class Post
-  
   attr_accessor :title, :author
-  
   @@all = [] 
-  
   def initialize(title)
     @title = title
     save
   end 
-  
   def save
      @@all << self 
   end 
-  
   def self.all
     @@all
   end
-  
   def author_name
       self.author.nil?? nil : self.author.name
   end 
